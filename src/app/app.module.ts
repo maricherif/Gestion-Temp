979d7cbe36170fe42177a63cd75d9166a8435133
fadal ba <fadalba@gmail.com>
2023-01-02 15:56:04 +0000

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AjoutUsersComponent } from './ajout-users/ajout-users.component';
+import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    AjoutUsersComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+  
+
   ],
   providers: [],
   bootstrap: [AppComponent]
